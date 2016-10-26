548fbd44c6fe3d34fd1d061cb0e653a08de50a0d
Jean Fabre <contact@fabrejean.net>
2016-10-26 10:55:24 +0300

    safe commit

---
+++ Assets/BugTest/BugAction.cs
@@ -0,0 +1,29 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+
+	public class BugAction : FsmStateAction
+	{
+
+		public FsmString myFsmString = new FsmString();
+		
+		public override void Reset()
+		{
+		}
+		
+		public override void OnEnter()
+		{
+			if (myFsmString!=null)
+			{
+			UnityEngine.Debug.Log(myFsmString.Value);
+			}else{
+				UnityEngine.Debug.Log("myFsmString is null");
+			}
+
+			Finish ();
+		}
+	}
+}
---
+++ Assets/BugTest/Editor/BugActionCustomEditor.cs
@@ -0,0 +1,44 @@
+﻿using HutongGames.PlayMaker.Actions;
+using HutongGames.PlayMakerEditor;
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+using System.Reflection;
+using HutongGames.PlayMaker.Ecosystem.Utils;
+
+namespace HutongGames.PlayMakerEditor
+{
+	[CustomActionEditor(typeof(BugAction))]
+	public class BugActionCustomEditor : CustomActionEditor
+	{
+
+
+		public override bool OnGUI()
+		{
+			var _target = target as BugAction;
+
+			/*
+			var _editingObject_Field = typeof(ActionEditor).GetField("editingObject", 
+			                                                         BindingFlags.Static | 
+			                                                         BindingFlags.NonPublic);
+			_editingObject_Field.SetValue(null, _target);
+			
+			var _editingField_Field = typeof(ActionEditor).GetField("editingField", 
+			                                                        BindingFlags.Static | 
+			                                                        BindingFlags.NonPublic);
+			_editingField_Field.SetValue(null, _target.GetType().GetField("myFsmString"));
+			*/
+			
+			//ActionEditor.editingObject = _target;
+			//ActionEditor.editingField = _target.GetType().GetField("myFsmString");
+
+			PlayMakerInspectorUtils.SetActionEditorVariableSelectionContext(_target,_target.GetType().GetField("myFsmString"));
+			_target.myFsmString = VariableEditor.FsmStringField(new GUIContent("Fsm String"),_target.Fsm,_target.myFsmString,null);
+			
+			return GUI.changed;
+		}
+
+
+	}
+}
