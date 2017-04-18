d33894c92ede186b6fac6c10299cc751c5cf31e7
Jean Fabre <contact@fabrejean.net>
2017-04-18 12:13:04 +0300

    updated sub modules, safe commit

---
+++ Assets/ArrayCountBug/PropArrayBugAction.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	public class test {
+		
+		public string property;
+		public bool variable;
+	}
+	
+	
+	public class PropArrayBugAction : FsmStateAction
+	{
+
+
+		public test[] properties;
+
+		
+		
+		public override void Reset ()
+		{
+			
+			properties = null;
+
+
+		}
+		
+		public override void OnEnter ()
+		{
+			
+			SelectSingleNode();
+			
+			Finish ();
+		}
+		
+		
+		private void SelectSingleNode ()
+		{
+
+			foreach (test prop in properties) 
+			{
+				UnityEngine.Debug.Log(prop.property+" "+prop.variable);
+
+			}
+
+			Finish ();
+		}
+		
+	}
+}
